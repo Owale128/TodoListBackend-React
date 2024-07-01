46df0e5bf6c75369c8b3a0301ae55b1648efc9b3
Owale128 <Christian.buaka@hotmail.com>
2024-07-01 12:17:32 +0200

    Added some more endpoints and also connected to mongoDB

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const connectDB = require('./database/db')
 const Todo = require('./model/todoModel')
 
-app.use(bodyParser.json());
+connectDB()
 app.use(cors())
+app.use(bodyParser.json());
+
 
 app.get('/', async (req, res) => {
-
     try {
         const todos = await Todo.find();
         res.json(todos);
@@ -24,7 +26,6 @@ app.post('/api/post', async (req, res) => {
         title: req.body.title,
         completed: req.body.completed
     });
-
     try {
         const newTodo = await todo.save()
         res.status(201).json(newTodo);
@@ -33,6 +34,30 @@ app.post('/api/post', async (req, res) => {
     }
 });
 
+app.put('/api/todos/:id/complete', async(req, res)=> {
+    const id = req.params.id;
+    try {
+        const todo = await Todo.findById(id);
+        if(!todo) {
+            return res.status(404).json({ message: 'Todo not found'});
+        }
+        todo.completed = !todo.completed;
+        await todo.save();
+        res.json({ message: 'Todo marked as completed', todo})
+    } catch (err) {
+        res.status(400).json({ message: err.message})
+    }
+});
+
+app.delete('/api/delete/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        await Todo.findByIdAndDelete(id);
+        res.sendStatus(204);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
